0cb5e40d28dca2c5a112926b5d84f2a498c77db7
David Ihim <ihimdavid@gmail.com>
2023-05-16 15:24:25 -0700

    add arm controller

--- redis_keys.h
+++ redis_keys.h
@@ -14,5 +14,15 @@ const std::string JOINT_VELOCITIES_KEY = "sai2::cs225a::project::sensors::dq";
 const std::string JOINT_TORQUES_COMMANDED_KEY = "sai2::cs225a::project::actuators::fgc";
 const std::string X_VEL_KEY = "sai2::cs225a::project::control::xv";
 const std::string Y_VEL_KEY = "sai2::cs225a::project::control::yv";
+const std::string CONTROLLER_RUNNING_KEY = "sai2::sim::panda::controller";
+const std::string GRIPPER_JOINT_ANGLES_KEY = "sai2::sim::panda_gripper::sensors::q";
+const std::string GRIPPER_JOINT_VELOCITIES_KEY = "sai2::sim::panda_gripper::sensors::dq";
 
+const std::string EE_X_POS_KEY = "sai2::cs225a::project::control::xp";
+const std::string EE_Y_POS_KEY = "sai2::cs225a::project::control::yp";
+const std::string EE_Z_POS_KEY = "sai2::cs225a::project::control::zp";
+const std::string ORI_X_POS_KEY = "sai2::cs225a::project::control::xo";
+const std::string ORI_Y_POS_KEY = "sai2::cs225a::project::control::yo";
+const std::string ORI_Z_POS_KEY = "sai2::cs225a::project::control::zo";
+const std::string GRIPPER_POS_KEY = "sai2::cs225a::project::control::gp";
 
